bf8086c22e20eeba702c985897166288d6433ed9
Nathan Rajlich <nathan@tootallnate.net>
2011-03-23 14:30:20 -0700

    Make 'parseContentType' be able to parse key/value pairs other than just 'boundary'.

--- parseContentType.js
+++ parseContentType.js
@@ -1,16 +1,17 @@
-var boundary = 'boundary';
 function parseContentType(contentType) {
   var rtn = {};
   if (!contentType || typeof contentType !== 'string') return rtn;
   var parts = contentType.split(';');
   for (var i=0, l=parts.length; i<l; i++) {
     var part = parts[i].trim();
-    if (part.indexOf(boundary) === 0) {
-      part = part.substring(boundary.length+1);
+    var firstEquals = part.indexOf('=');
+    if (firstEquals > 0) {
+      var name = part.substring(0, firstEquals);
+      part = part.substring(firstEquals+1);
       if (part[0] === '"' && part[part.length-1] === '"') {
         part = part.substring(1, part.length-1);
       }
-      rtn.boundary = part;
+      rtn[name] = part;
     } else if (part.indexOf('/') > 0) {
       part = part.split('/');
       rtn.type = part[0];
