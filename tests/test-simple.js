f7300cd493c3314b93544b275fe1bf78948e2ad1
Nathan Rajlich <nathan@tootallnate.net>
2011-03-24 10:49:42 -0700

    Finish up the test case. The asserts for the bodies of the two message bodies are still disabled until headers are supported.

--- tests/test-simple.js
+++ tests/test-simple.js
@@ -1,6 +1,7 @@
 var fs = require('fs');
 var assert = require('assert');
 var Parser = require('../parser');
+var inspect = require('util').inspect;
 
 var stream = fs.createReadStream(__dirname + '/dumps/simple.dump', {
   bufferSize: 1
@@ -15,6 +16,7 @@ parser.on('preamble', function(preamble) {
     text += chunk;
   });
   preamble.on('end', function() {
+    console.error('Preamble:', inspect(text));
     assert.equal(text, 'This is the preamble.  It is to be ignored, though it\r\nis a handy place for mail composers to include an\r\nexplanatory note to non-MIME compliant readers.');
   });
 });
@@ -27,7 +29,7 @@ parser.on('part', function(part) {
     text += chunk;
   });
   part.on('end', function() {
-    console.error(num, text);
+    console.error('Part #'+num+':', inspect(text));
     switch(num) {
       case 0:
         //assert.equal(text, 'This is implicitly typed plain ASCII text.\r\nIt does NOT end with a linebreak.');
@@ -47,12 +49,13 @@ parser.on('epilogue', function(epilogue) {
     text += chunk;
   });
   epilogue.on('end', function() {
-    assert.equal(text, 'This is the epilogue.  It is also to be ignored.');
+    console.error('Epilogue: ', inspect(text));
+    assert.equal(text, 'This is the epilogue.  It is also to be ignored.\r\n');
   });
 });
 
 process.on('exit', function() {
   assert.ok(gotPreamble);
-  //assert.ok(gotEpilogue);
-  //assert.equal(partCount, 2);
+  assert.ok(gotEpilogue);
+  assert.equal(partCount, 2);
 });
