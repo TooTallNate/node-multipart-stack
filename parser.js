0ffb5c4f0741d1fd8bf1da8828f8d2f85f5f00dd
Nathan Rajlich <nathan@tootallnate.net>
2011-03-23 17:59:57 -0700

    Some real progress. At least I have the streaming parsing logic thought out I hope.

--- parser.js
+++ parser.js
@@ -15,8 +15,23 @@ function Parser(stream, boundary) {
   EventEmitter.call(this);
   this.stream = stream;
   this.boundary = boundary;
-  this.currentPart = new PartParser(this, false);
   this._started = false;
+  this.currentPart = new PartParser(this, false);
+  this.once('_start', this._onStart);
+
+  // We have to nextTick emitting 'preamble', since we have to give time
+  // for the user to attach an event handler for the event.
+  var self = this;
+  process.nextTick(function() {
+    self.emit('preamble', self.currentPart);
+    self.emit('_start');
+  });
 }
 inherits(Parser, EventEmitter);
 module.exports = Parser;
+
+// Called after the initial nextTick after Parser construction. This event
+// is called after we can assume the user has set up their event listeners. 
+Parser.prototype._onStart = function onStart() {
+ this._started = true;
+}
--- partParser.js
+++ partParser.js
@@ -1,3 +1,5 @@
+require('bufferjs');
+var BufferList = require('bufferlist');
 var StreamStack = require('stream-stack').StreamStack;
 var inherits = require('util').inherits;
 
@@ -9,9 +11,62 @@ var inherits = require('util').inherits;
  * 'preamble' and 'epilogue')
  */
 function PartParser(parent, parseHeaders) {
-  StreamStack.call(this, parent, {
-
+  StreamStack.call(this, parent.stream, {
+    data: function onData(chunk) {
+      this._onData(chunk);
+    }
   });
+  this.parent = parent;
+  this._buffers = new BufferList();
+  if (parent._started) {
+    this._onData = parseHeaders ? this._parseHeaders : this._parseBody;
+  } else {
+    // We just have to buffer any incoming data until the Parser starts.
+    this._onData = this._bufferData;
+    var self = this;
+    parent.on('_start', function() {
+      self._onData = parseHeaders ? self._parseHeaders : self._parseBody;
+      var buf = self._buffers.take();
+      self._buffers.advance(buf.length);
+      self.emit('data', buf);
+    });
+  }
 }
 inherits(PartParser, StreamStack);
 module.exports = PartParser;
+
+
+PartParser.prototype._bufferData = function bufferData(chunk) {
+  this._buffers.push(chunk);
+}
+
+PartParser.prototype._parseHeaders = function parseHeaders(chunk) {
+}
+
+// Parsing Logic:
+//  -Add the current chunk to the bufferlist if necessary.
+//  -Check to see if the beginning of the bufferlist looks like a boundary:
+//     If the bufferlist.length < endingBoundary.length+1, wait for next data event
+//     If bufferlist.length >= endingBoundary+1, check if there's a boundary:
+//       Check endingBoundary, then regularBoundary
+//         If yes, emit 'end' on this PartParser
+//         If no, we can safely emit what we have so far as 'data'
+//  -If the current bufferlist contains the beginning of a boundary,
+//   then slice the Buffer and emit the data to the user up to the
+//   beginning of the "potential" boundary.
+//  -Repeat from step 1, checking for a boundary again, etc.
+PartParser.prototype._parseBody = function parseBody(chunk) {
+  if (chunk) {
+    this._bufferData(chunk);
+  }
+}
+
+
+// If the parent Stream ends, then:
+//   1) We're parsing a body part, and anything in the buffers should be emitted
+//      to the user, up to the ending boundary which should be at the end.
+//   2) We're parsing the epilogue, and anything in the buffers should be emitted
+//      to the user. There will be no ending boundary to parse out.
+PartParser.prototype._onEnd = function onEnd() {
+  this.emit('end');
+}
